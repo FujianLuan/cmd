e70b1dada003e22a0ce9043e4a4ab61952a87aac
luanfujian <1043912811@qq.com>
2017-02-13 17:14:37 +0800

    Add files via upload

---
+++ JavaSetting.bat
@@ -0,0 +1,10 @@
+@echo off
+rem set the envir
+set %os%
+echo ************************************
+set %path%
+echo ************************************
+set %JAVA_HOME%
+echo ************************************
+set %CLASS_PATH%
+pause
